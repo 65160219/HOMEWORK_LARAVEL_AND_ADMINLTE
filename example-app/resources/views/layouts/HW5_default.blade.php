9a9fe41bcd1819bff15b91537228df6c29d7cedc
65160219 <152172067+65160219@users.noreply.github.com>
2024-01-15 22:05:18 +0700

    update code

--- example-app/resources/views/layouts/HW5_default.blade.php
+++ example-app/resources/views/layouts/HW5_default.blade.php
@@ -164,9 +164,9 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ url('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                <img src="{{ url('assets/dist/img/SE BUU.jpg') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">SE CAMP</span>
             </a>
 
             <!-- Sidebar -->
@@ -174,11 +174,11 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ url('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                        <img src="{{ url('assets/dist/img/UserLucky.jpg') }}" class="img-circle elevation-2"
                             alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">65160219</a>
                     </div>
                 </div>
 
